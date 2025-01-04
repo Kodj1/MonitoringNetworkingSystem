c48f7ed2e1e11f674b0a707792f392122d3a22ae
Kodj1 <veretennikov20@yandex.ru>
2025-01-05 00:24:12 +0300

    Host

--- Menu.h
+++ Menu.h
@@ -1,11 +1,3 @@
-/********************************************************************************
-** Form generated from reading UI file 'designervvHGxU.ui'
-**
-** Created by: Qt User Interface Compiler version 5.15.15
-**
-** WARNING! All changes made in this file will be lost when recompiling UI file!
-********************************************************************************/
-
 #ifndef MENU_H
 #define MENU_H
 
