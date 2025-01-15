85186e8caa29cd8154db22695552353eb14c6d09
Tanj1 <veretennikov20@yandex.ru>
2025-01-15 22:15:09 +0300

    SQL server

--- client.sh
+++ client.sh
@@ -1,21 +1,42 @@
 #!/bin/bash
 
-# Адрес и порт сервера
-SERVER_IP="127.0.0.1"
-SERVER_PORT=8080
+# Собрать информацию об имени хоста и IP-адресе
+hostname=$(hostname)
+ip_address=$(hostname -I | awk '{print $1}')
 
-# Пример данных в формате JSON
-json_data=$(cat <<EOF
+# Собрать информацию о загрузке процессора
+cpu_usage=$(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}')
+
+# Собрать информацию об использовании оперативной памяти
+memory_usage=$(free -m | awk 'NR==2{printf "%.2f", $3*100/$2 }')
+
+# Собрать информацию о сетевом трафике
+network_in=$(cat /proc/net/dev | grep eth0 | awk '{print $2}')
+network_out=$(cat /proc/net/dev | grep eth0 | awk '{print $10}')
+
+# Собрать всю информацию в JSON-формате
+data=$(cat <<EOF
 {
-  "node_name": "Node1",
-  "ip_address": "192.168.1.1",
-  "cpu_usage": 10,
-  "memory_usage": 2048,
-  "network_in": 300,
-  "network_out": 200
+    "node_name": "$hostname",
+    "ip_address": "$ip_address",
+    "cpu_usage": "$cpu_usage",
+    "memory_usage": "$memory_usage",
+    "network_in": "$network_in",
+    "network_out": "$network_out"
 }
 EOF
 )
 
-# Отправка данных на сервер с использованием nc
-echo "$json_data" | nc $SERVER_IP $SERVER_PORT
+# IP-адрес и порт получателя
+recipient_ip="192.168.0.3"
+recipient_port="8080"
+
+# Отправить данные на другой IP-адрес с использованием netcat
+echo "$data" | nc $recipient_ip $recipient_port
+
+# Вывести результат отправки
+if [ $? -eq 0 ]; then
+    echo "Данные успешно отправлены на $recipient_ip:$recipient_port"
+else
+    echo "Ошибка при отправке данных на $recipient_ip:$recipient_port"
+fi
---
+++ server.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+
+
+# Порт для прослушивания входящих данных
+LISTEN_PORT=8080
+
+# Функция для обработки входящих данных
+handle_data() {
+    # Принять данные из файла
+    data_file="received_metrics.txt"
+    cat > $data_file
+    
+# Чтение данных из файла
+    hostname=$(grep "hostname=" $data_file | cut -d'=' -f2)
+    ip_address=$(grep "ip_address=" $data_file | cut -d'=' -f2)
+    cpu_usage=$(grep "cpu_usage=" $data_file | cut -d'=' -f2)
+    memory_usage=$(grep "memory_usage=" $data_file | cut -d'=' -f2)
+    network_in=$(grep "network_in=" $data_file | cut -d'=' -f2)
+    network_out=$(grep "network_out=" $data_file | cut -d'=' -f2)
+    collected_at=$(date +'%Y-%m-%d %H:%M:%S')
+    
+    # Формирование SQL-запроса для вставки данных в таблицу metrics
+    SQL_QUERY=$(cat <<EOF
+WITH node AS (
+    INSERT INTO nodes (name, ip_address)
+    VALUES ('$node_name', '$ip_address')
+    ON CONFLICT (ip_address) DO UPDATE SET name = EXCLUDED.name
+    RETURNING id
+)
+INSERT INTO metrics (node_id, cpu_usage, memory_usage, network_in, network_out, collected_at)
+VALUES ((SELECT id FROM node), $cpu_usage, $memory_usage, $network_in, $network_out, '$collected_at');
+EOF
+)
+
+    # Выполнение SQL-запроса с использованием psql
+    echo "$SQL_QUERY" | PGPASSWORD="12345" psql -h 127.0.0.1 -d mns -U postgres
+
+    # Проверка на успешное выполнение
+    if [ $? -eq 0 ]; then
+        echo "Данные успешно записаны в базу данных"
+    else
+        echo "Ошибка при записи данных в базу данных"
+    fi
+}
+
+# Прослушивание порта и обработка входящих данных
+while true; do
+    nc -l -p $LISTEN_PORT -c 'handle_data'
+done
